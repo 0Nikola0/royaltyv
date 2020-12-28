b1695a6d37dd5fce44af192209009aa7b3b61842
0Nikola0 <62570597+0Nikola0@users.noreply.github.com>
2020-12-28 21:51:32 +0100

    Added Users urls and crispy

--- royalty/settings.py
+++ royalty/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'royaltyv.apps.RoyaltyvConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
