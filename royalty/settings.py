53ebab200f0c3a2c431c560e005e4ca14622f0db
0Nikola0 <62570597+0Nikola0@users.noreply.github.com>
2021-01-01 19:18:08 +0100

    fixed heroku bugs

--- royalty/settings.py
+++ royalty/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+# This doesn't matter if DEBUG=True
+ALLOWED_HOSTS = [
+    'herokuapp.com'
+]
 
 
 # Application definition
